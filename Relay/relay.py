0aaf710c6e9877241eb86e5e3aaa84380b9c6a5d
James Howard <jhhoward@gmail.com>
2019-12-03 16:03:08 +0000

    Made relay script more Linux friendly and ignore internal ports for Raspberry Pi

--- Relay/relay.py
+++ Relay/relay.py
@@ -1,21 +1,21 @@
 # Requires pySerial and keyboard
-# python -m pip install pyserial keyboard
+# python -m pip install pyserial
 
 import serial
 import serial.tools.list_ports
-import keyboard
 import socket
 import select
 
 LISTEN_PORT = 9999
 VERBOSE = False
 MAX_CONNECTIONS = 2
+IGNORED_PORTS = [ '/dev/ttyAMA0' ]
 
 activeSerialPorts = {}
 activeSockets = []
 
 
-print("Starting relay. Press q to stop")
+print("Starting relay. Press Ctrl-C to stop")
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -47,69 +47,77 @@ def sendData(source, data):
 		if (sock != source) and (sock != serverSocket):
 			sock.send(data)
 
-while True:
-	if keyboard.is_pressed('q'):
-		break
-	
-	# Check for new serial ports
-	if numActiveConnections() < MAX_CONNECTIONS:
-		availablePorts = serial.tools.list_ports.comports()
-		
-		for port in availablePorts:
-			if not port.device in activeSerialPorts:
-				try:
-					activeSerialPorts[str(port.device)] = serial.Serial(port.device, 9600)
-					print("Opened port " + str(port.device))
-					printStatus()
-				except:
-					continue
-	
-	# Process serial ports
-	disconnectedSerialPorts = []
-	for portName in activeSerialPorts:
-		try:
-			serialPort = activeSerialPorts[portName]
-			if serialPort.in_waiting > 0:
-				inData = serialPort.read()
-				sendData(serialPort, inData)
-		except:
-			disconnectedSerialPorts.append(portName)
+running = True
 
-	# Remove closed serial ports
-	for portName in disconnectedSerialPorts:
-		print("Lost connection to", portName)
-		activeSerialPorts.pop(portName)
-		printStatus()
+try:
+	while running:
+		# Check for new serial ports
+		if numActiveConnections() < MAX_CONNECTIONS:
+			availablePorts = serial.tools.list_ports.comports()
+			
+			for port in availablePorts:
+				if not port.device in activeSerialPorts and not port.device in IGNORED_PORTS:
+					try:
+						activeSerialPorts[str(port.device)] = serial.Serial(port.device, 9600)
+						print("Opened port " + str(port.device))
+						printStatus()
+					except KeyboardInterrupt:
+						raise
+					except:
+						continue
 		
-	# Check for socket connections
-	socketsToClose = []
-	readable, writable, errored = select.select(activeSockets, [], [], 0)
-	for s in readable:
-		if s is serverSocket:
-			clientSocket, address = serverSocket.accept()
-			if numActiveConnections() < MAX_CONNECTIONS:
-				activeSockets.append(clientSocket)
-				print("Connection from", address)
-				printStatus()
-			else:
-				print("Rejecting connection from", address, "- too many connections!")
-				clientSocket.close()
-		else:
+		# Process serial ports
+		disconnectedSerialPorts = []
+		for portName in activeSerialPorts:
 			try:
-				data = s.recv(1)
-				if data:
-					sendData(s, data)
-				else:
-					socketsToClose.append(s)
+				serialPort = activeSerialPorts[portName]
+				if serialPort.in_waiting > 0:
+					inData = serialPort.read()
+					sendData(serialPort, inData)
+			except KeyboardInterrupt:
+				raise
 			except:
-				socketsToClose.append(s)
-	
-	for s in socketsToClose:
-		print("Closing socket", s.getpeername())
-		s.close()
-		activeSockets.remove(s)
-		printStatus()
-				
+				disconnectedSerialPorts.append(portName)
+
+		# Remove closed serial ports
+		for portName in disconnectedSerialPorts:
+			print("Lost connection to", portName)
+			activeSerialPorts.pop(portName)
+			printStatus()
+			
+		# Check for socket connections
+		socketsToClose = []
+		readable, writable, errored = select.select(activeSockets, [], [], 0)
+		for s in readable:
+			if s is serverSocket:
+				clientSocket, address = serverSocket.accept()
+				if numActiveConnections() < MAX_CONNECTIONS:
+					activeSockets.append(clientSocket)
+					print("Connection from", address)
+					printStatus()
+				else:
+					print("Rejecting connection from", address, "- too many connections!")
+					clientSocket.close()
+			else:
+				try:
+					data = s.recv(1)
+					if data:
+						sendData(s, data)
+					else:
+						socketsToClose.append(s)
+				except KeyboardInterrupt:
+					raise
+				except:
+					socketsToClose.append(s)
+		
+		for s in socketsToClose:
+			print("Closing socket", s.getpeername())
+			s.close()
+			activeSockets.remove(s)
+			printStatus()
+except KeyboardInterrupt:
+	running = False
+
 print("Shutting down..")
 		
 # Cleanup serial ports
